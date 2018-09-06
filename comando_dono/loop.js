0879027a49feb121b2b2fdb21ba5295246989307
ImSuperZin <39243594+ImSuperZin@users.noreply.github.com>
2018-09-06 11:10:01 -0300

    Update loop.js

--- comando_dono/loop.js
+++ comando_dono/loop.js
@@ -13,11 +13,11 @@ module.exports.run = async (client) =>{
 
     for (i=0; i<10;){
         
-        client.user.setPresence({ game: { name: `Estou em ${client.guilds.size} Grupos obrigado Galera!`, url: "https://www.twitch.tv/viniciussz_", type: 1} });
+        client.user.setPresence({ game: { name: `${client.guilds.size} Grupos Me Usando`, url: "https://www.twitch.tv/viniciussz_", type: 1} });
         await sleep(60000)
         client.user.setPresence({ game: { name: "z!ajuda | z!discord", type: 0 } });
         await sleep(60000)
-        client.user.setPresence({ game: { name: `${client.users.size} Me Usarem! ðŸ˜`, type: 3} });
+        client.user.setPresence({ game: { name: `${client.users.size} Usuarios Me Usarem! ðŸ˜`, type: 3} });
         await sleep(60000)
     }
    
